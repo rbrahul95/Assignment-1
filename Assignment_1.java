f34e6a70dd3c70acadd8003c02a6ae0704c96896
rbrahul95 <rbrahul95@gmail.com>
2016-08-01 12:59:32 +0530

    Add files via upload

---
+++ Assignment_1.java
@@ -0,0 +1,14 @@
+
+public class Assignment_1 {
+    
+    public static void main(String[] args) {
+       
+     int a=5;
+     float b=(float) 5.5;
+     int sum=(int) (a+b);
+        System.out.println("Sum is: "+sum);
+ 
+            
+    }
+    
+}
